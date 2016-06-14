7017aac4ac7e59d9afe2ee75ec78bfcc04eb3403
Will Cosulich <william.cosulich@gmail.com>
2016-06-14 07:51:28 -0400

    Commit Task 3 Part A
    Created a responsive to do list app.

--- todolist/db/seeds.rb
+++ todolist/db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Task.delete_all
+
+Task.create(:task => 'Task 1', :due_date => '2016-06-14')
+Task.create(:task => 'Task 2', :due_date => '2016-06-15')
+Task.create(:task => 'Task 3', :due_date => '2016-06-15')
+Task.create(:task => 'Task 4', :due_date => '2016-06-20')
+Task.create(:task => 'Task 5', :due_date => '2016-06-20')
+
